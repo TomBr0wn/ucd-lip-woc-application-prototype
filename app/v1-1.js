3fa87e2db559f9db325fd92d54dfd4caa77ff869
Tom <thomas.brown1@digital.hmrc.gov.uk>
2022-08-17 12:36:33 +0100

    Fixed v1-1/defendant-phone-number-confirmed.html Panels now say UPDATE or CONFIRM

--- app/v1-1.js
+++ app/v1-1.js
@@ -9,134 +9,134 @@ const router = express.Router();
 // });
 
 // Run this code when a form is submitted to 'user-type'
-router.post("/recovery-method", function (req, res) {
-	// Make a variable and give it the value from 'recovery-method'
-	var recoveryMethod = req.session.data["recovery-method"];
-	console.log(recoveryMethod);
-	// Check whether recoveryMethod
-	if (recoveryMethod == "charging order") {
-		// Send user to next page
-		res.redirect("charging-order");
-	}
-	if (recoveryMethod == "warrant or writ") {
-		// Send user to next page
-		res.redirect("choose-warrant-or-writ-of-control");
-	}
-	if (recoveryMethod == "attachment earnings order") {
-		// Send user to next page
-		res.redirect("attachment-of-earnings-order");
-	}
-	if (recoveryMethod == "third party debt") {
-		// Send user to next page
-		res.redirect("third-party-debt-order");
-	}
-	else {
-		// Send user to trade page
-		res.redirect("help-with-fees");
-	}
+router.post("/recovery-method", function(req, res) {
+    // Make a variable and give it the value from 'recovery-method'
+    var recoveryMethod = req.session.data["recovery-method"];
+    console.log(recoveryMethod);
+    // Check whether recoveryMethod
+    if (recoveryMethod == "charging order") {
+        // Send user to next page
+        res.redirect("charging-order");
+    }
+    if (recoveryMethod == "warrant or writ") {
+        // Send user to next page
+        res.redirect("choose-warrant-or-writ-of-control");
+    }
+    if (recoveryMethod == "attachment earnings order") {
+        // Send user to next page
+        res.redirect("attachment-of-earnings-order");
+    }
+    if (recoveryMethod == "third party debt") {
+        // Send user to next page
+        res.redirect("third-party-debt-order");
+    } else {
+        // Send user to trade page
+        res.redirect("help-with-fees");
+    }
 });
 
 // Run this code when a form is submitted to 'help-with-fees'
-router.post("/had-help-with-fees", function (req, res) {
-	// Make a variable and give it the value from 'help-with-fees'
-	var feeHelp = req.session.data["help-with-fees"];
-	console.log(feeHelp);
-	// Check whether feeHelp
-	if (feeHelp == "No") {
-		// Send user to next page
-		res.redirect("has-defendant-paid-any");
-		
-	} if (feeHelp == "Yes") {
-		// Send user to next page
-		res.redirect("help-with-fees-yes");
-		
-	} else {
-		// Send user to trade page
-		res.redirect("#");
-	}
+router.post("/had-help-with-fees", function(req, res) {
+    // Make a variable and give it the value from 'help-with-fees'
+    var feeHelp = req.session.data["help-with-fees"];
+    console.log(feeHelp);
+    // Check whether feeHelp
+    if (feeHelp == "No") {
+        // Send user to next page
+        res.redirect("has-defendant-paid-any");
+
+    }
+    if (feeHelp == "Yes") {
+        // Send user to next page
+        res.redirect("help-with-fees-yes");
+
+    } else {
+        // Send user to trade page
+        res.redirect("#");
+    }
 });
 
 // Run this code when a form is submitted to 'help-with-fees'
-router.post("/paid-any", function (req, res) {
-	// Make a variable and give it the value from 'help-with-fees'
-	var feeHelp = req.session.data["defendant-paid-already"];
-	console.log(feeHelp);
-	// Check whether feeHelp
-	if (feeHelp == "No") {
-		// Send user to next page
-		res.redirect("defendant-paid-no");
-	} else {
-		// Send user to trade page
-		res.redirect("how-much-do-you-want-to-recover");
-	}
+router.post("/paid-any", function(req, res) {
+    // Make a variable and give it the value from 'help-with-fees'
+    var feeHelp = req.session.data["defendant-paid-already"];
+    console.log(feeHelp);
+    // Check whether feeHelp
+    if (feeHelp == "No") {
+        // Send user to next page
+        res.redirect("defendant-paid-no");
+    } else {
+        // Send user to trade page
+        res.redirect("how-much-do-you-want-to-recover");
+    }
 });
 
 
 // Run this code when a form is submitted to 'help-with-fees'
-router.post("/is-recovery-greater-than-judgement", function (req, res) {
-	// Make a variable and give it the value from 'help-with-fees'
-	var judgementAmount = parseInt(req.session.data["judgement-amount"]);
-	var recoveryAmount = parseInt(req.session.data["recovery-amount"]);
-	console.log(judgementAmount);
-	console.log(recoveryAmount);
-	// Check whether feeHelp
-	if (recoveryAmount > judgementAmount) {
-		// Send user to next page
-		res.redirect("are-you-claiming-additional-costs");
-	} else {
-		// Send user to trade page
-		res.redirect("defendant-address");
-	}
+router.post("/is-recovery-greater-than-judgement", function(req, res) {
+    // Make a variable and give it the value from 'help-with-fees'
+    var judgementAmount = parseInt(req.session.data["judgement-amount"]);
+    var recoveryAmount = parseInt(req.session.data["recovery-amount"]);
+    console.log(judgementAmount);
+    console.log(recoveryAmount);
+    // Check whether feeHelp
+    if (recoveryAmount > judgementAmount) {
+        // Send user to next page
+        res.redirect("are-you-claiming-additional-costs");
+    } else {
+        // Send user to trade page
+        res.redirect("defendant-address");
+    }
 });
 
 
 // Run this code when a form is submitted to 'check-address
-router.post("/check-address", function (req, res) {
-	// Make a variable and give it the value from 'check-address
-	var knowAddress = req.session.data["defendant-address"];
-	console.log(knowAddress);
-	// Check whether feeHelp
-	if (knowAddress == "no") {
-		// Send user to next page
-		res.redirect("defendant-address-unknown");
-	} 
-	if (knowAddress == "yes") {
-		// Send user to next page
-		res.redirect("defendant-address-confirmed");
-	}
+router.post("/check-address", function(req, res) {
+    // Make a variable and give it the value from 'check-address
+    var knowAddress = req.session.data["defendant-address"];
+    console.log(knowAddress);
+    // Check whether feeHelp
+    if (knowAddress == "no") {
+        // Send user to next page
+        res.redirect("defendant-address-unknown");
+    }
+    if (knowAddress == "yes") {
+        // Send user to next page
+        res.redirect("defendant-address-confirmed");
+    }
 });
 
 // Run this code when a form is submitted to 'check-phone-number
-router.post("/check-phone-number", function (req, res) {
-	// Make a variable and give it the value from 'check-phone-number
-	var knowPhoneNum = req.session.data["defendant-phone"];
-	console.log(knowPhoneNum);
-	// Check whether feeHelp
-	if (knowPhoneNum == "no") {
-		// Send user to next page
-		res.redirect("defendant-phone-number-unknown");
-	} 
-	if (knowPhoneNum == "yes") {
-		// Send user to next page
-		res.redirect("defendant-phone-number-confirmed");
-	}
+router.post("/check-phone-number", function(req, res) {
+    // Make a variable and give it the value from 'check-phone-number
+    var knowPhoneNum = req.session.data["defendant-phone"];
+    console.log(knowPhoneNum);
+    // Check whether feeHelp
+    if (knowPhoneNum == "no") {
+        // Send user to next page
+        res.redirect("defendant-phone-number-unknown");
+    }
+    if (knowPhoneNum == "yes") {
+        // Send user to next page
+        res.redirect("defendant-phone-number-confirmed");
+    }
 });
 
-router.post("/change-amount", function (req, res) {
-	var newAmount = req.session.data["try-to-recover"];
-	console.log(newAmount);
+router.post("/change-amount", function(req, res) {
+    var newAmount = req.session.data["try-to-recover"];
+    console.log(newAmount);
 })
 
-router.post("/is-manual-check-needed", function (req, res) {
-	var judgementAmount = parseInt(req.session.data["judgement-amount"]);
-	var recoveryAmount = parseInt(req.session.data["recovery-amount"]);
-	console.log(judgementAmount);
-	console.log(recoveryAmount);
-	if (recoveryAmount <= judgementAmount) {
-		res.redirect("manual-check-no");
-	} else {
-		res.redirect("manual-check-yes");
-	}
+router.post("/is-manual-check-needed", function(req, res) {
+    var judgementAmount = parseInt(req.session.data["judgement-amount"]);
+    var recoveryAmount = parseInt(req.session.data["recovery-amount"]);
+    console.log(judgementAmount);
+    console.log(recoveryAmount);
+    if (recoveryAmount <= judgementAmount) {
+        res.redirect("manual-check-no");
+    } else {
+        res.redirect("manual-check-yes");
+    }
 })
 
 
